77002b521909d47d39f91ff8a3f37dbe8037762b
Wojciech Prokopek <w.prokopek@gmail.com>
2022-06-23 14:51:39 +0200

    adding NEXTAUTH_SECRET field

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,8 +12,11 @@ export default NextAuth({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET
   })
+  
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/auth/signin',
   }
+  
 })
