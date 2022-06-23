167de4c4b865190068bc1341cba28560586503a2
Wojciech Prokopek <w.prokopek@gmail.com>
2022-06-23 19:53:58 +0200

    facebook auth provider disabled

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,10 +4,10 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-  FacebookProvider({
-    clientId: process.env.FACEBOOK_CLIENT_ID,
-    clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-  }),
+  // FacebookProvider({
+  //   clientId: process.env.FACEBOOK_CLIENT_ID,
+  //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+  // }),
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET
